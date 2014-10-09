59cc56fc2de757fae12c31d0a27be16b6f379536
Robert Van Dell <awesomebobx64@gmail.com>
2014-10-08 19:08:56 -0700

    Digest authentication header is not ready yet.

--- classes/Http/Header/Request/Authorization/Digest.php
+++ classes/Http/Header/Request/Authorization/Digest.php
@@ -6,9 +6,6 @@ class Basic extends \Http\Header\Request\AbstractAuthorization
 {
     protected function _getCredentialsFromValue($credentialString)
     {
-        $decoded = base64_decode($credentialString);
-        $pieces  = explode(':', $decoded);
-
-        return ['userid' => $pieces[0], 'password' => $pieces[1]];
+        // Needs to be implemented
     }
 }
